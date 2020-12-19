a6c72342f7a89020b64513cb54e70c045b52c97e
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2020-12-19 11:12:46 +0530

    Add files via upload

---
+++ implement-strstr/28.implement-str-str.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=28 lang=cpp
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+class Solution {
+private:
+    bool check(string haystack, string needle, int idx){
+        for(int i=0;i<needle.length() ;i++){
+            if(haystack[idx++]!=needle[i])return false;
+        }
+        return true;
+    }
+
+public:
+    int strStr(string haystack, string needle) {
+        if(haystack.length()<needle.length())return -1;
+        else if(needle[0]=='\0')return 0;
+        for(int i=0;i<haystack.length();i++){
+            if(haystack.length()-i<needle.length())return -1;
+            if(haystack[i] == needle[0]){
+                if(check(haystack,needle,i))return i;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
