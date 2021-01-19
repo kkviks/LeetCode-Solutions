67d1869c53ce59ada4cde50a7f30b60423f7f55e
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-01-19 18:18:17 +0530

    Time: 48 ms (10.27%), Space: 8.9 MB (94.11%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+​
+// @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+​
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //Bruteforce Solution
+        vector<int> v;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                   v.push_back(i);
+                   v.push_back(j);
+                }
+            }
+        }
+        return v;
+    }
+};
+// @lc code=end
+​
+​
