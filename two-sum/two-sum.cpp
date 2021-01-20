6ad0c7b55a2afa7ef93efec1cb9571efac499e9a
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-01-20 08:54:10 +0530

    Time: 4 ms (99.08%), Space: 9.1 MB (85.74%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -10,16 +10,17 @@ using namespace std;
 ​
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        //Bruteforce Solution
+    vector<int> twoSum(vector<int> nums, int target) {
         vector<int> v;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                   v.push_back(i);
-                   v.push_back(j);
-                }
+        
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            if(m.count(target-nums[i])==1){
+                v.push_back(i);
+                v.push_back(m[target-nums[i]]);
+                break;
             }
+            else m[nums[i]]=i;
         }
         return v;
     }
