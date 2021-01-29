887849d08ef3598cd311e6e8e383444c86b39f48
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-01-30 00:37:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (91.64%) - LeetHub

--- to-lower-case/to-lower-case.cpp
+++ to-lower-case/to-lower-case.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    string toLowerCase(string str){
-        string ans = "";
-        
-        for(char ch: str){
-            if(ch>='A' and ch<='Z')ch += 32;
-            ans += ch;
-        }
-        
-        return ans; 
-    }
+    string toLowerCase(string str) {
+        string s = "";
+        
+        for(char ch:str){
+            if(ch>='A' and ch<='Z')ch = ch - 'A' + 'a';
+            s += ch;
+        }
+        
+        return s;
+    }
 };
