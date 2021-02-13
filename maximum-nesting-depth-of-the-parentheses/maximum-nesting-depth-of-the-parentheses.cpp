0eb99689ba678d1d5feaeffc472b0254cfeb1f1d
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-02-13 19:24:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (94.89%) - LeetHub

--- maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int maxDepth(string s) {
-        int maxSize = 0, currSize = 0;
-        
-        for(char ch: s){
-            if(ch=='('){
-                currSize++;
-                maxSize = max(maxSize,currSize);
-            }
-            else if(ch==')'){
-                currSize--;
-            }
-        }
-        
-        return maxSize;
-    }
+    int maxDepth(string s) {
+        int max_depth = INT_MIN;
+        int depth = 0;
+        for(char ch:s){
+            if(ch=='(')depth++;
+            else if(ch==')')depth--;
+            else;
+            max_depth = max(max_depth,depth);
+        }
+        max_depth = max(max_depth,depth);
+        return max_depth;
+    }
 };
