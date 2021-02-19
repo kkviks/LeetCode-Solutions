9940e31a6453075c55d26bdd6e86107c9e90d01b
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-02-19 18:34:58 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (73.30%) - LeetHub

--- shuffle-the-array/shuffle-the-array.cpp
+++ shuffle-the-array/shuffle-the-array.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> v;
-        v.reserve(2*n);
-        for(int i=0;i<n;i++){
-            v.push_back(nums[i]);
-            v.push_back(nums[i+n]);
-        }
-        return v;
-    }
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans;
+        int i = 0;
+        while(i<n){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
+            i++;
+        }
+        return ans;
+    }
 };
