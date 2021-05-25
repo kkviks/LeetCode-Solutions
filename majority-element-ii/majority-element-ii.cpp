b635eedf13109270597583a44c4a91d82f514707
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-05-25 16:00:24 +0530

    Time: 12 ms (72.04%), Space: 15.8 MB (44.08%) - LeetHub

--- majority-element-ii/majority-element-ii.cpp
+++ majority-element-ii/majority-element-ii.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //Bruteforce solution
-        int n = nums.size();
-        unordered_set<int> set;
-        for(int i=0;i<n;i++){
-            int x = nums[i];
-            if(set.count(x))continue;
-            int freq = 0;
-            for(int j=0;j<n;j++){
-                if(nums[j]==x)freq++;
+        
+        int e1 = INT_MIN, e2 = INT_MIN;
+        int c1 = 0, c2 = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(c1==0 and nums[i]!=e2){
+                e1 = nums[i];
             }
-            if(freq>n/3)set.insert(x);
+            if(c2==0 and nums[i]!=e1){
+                e2 = nums[i];
+            }
+            if(nums[i]==e1)c1++;
+            if(nums[i]==e2)c2++;
+            if(nums[i]!=e1 and nums[i]!=e2)c1--,c2--;
         }
-        vector<int> v(set.begin(),set.end());
+        
+        c1 = c2 = 0;
+        for(int i=0;i<nums.size();i++){
+            if(e1==nums[i])c1++;
+            if(e2==nums[i])c2++;
+        }
+        
+        vector<int> v;
+        if(c1>nums.size()/3)v.push_back(e1);
+        if(c2>nums.size()/3)v.push_back(e2);
         return v;
+        
     }
 };
