bc38d31bfd1bbcc0c902f0fc880c652329ce48ed
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-06-13 11:34:45 +0530

    Time: 200 ms (68.13%), Space: 114.1 MB (80.00%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.cpp
+++ palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,17 +9,35 @@
  * };
  */
 class Solution {
+private: 
+    ListNode* endOfFirstHalf(ListNode* head){
+        auto fast=head, slow = head;
+        while(fast->next!=NULL and fast->next->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    ListNode* reverseList(ListNode* head){
+        ListNode* prev = NULL, *curr = head;
+        while(curr!=NULL){
+            auto temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
 public:
     bool isPalindrome(ListNode* head) {
-        string s = "";
-        while(head!=NULL){
-            s+=to_string(head->val);
-            head = head->next;
-        }
-        int i=0,j= s.size()-1;
-        while(i<j){
-            if(s[i]!=s[j])return false;
-            i++, j--;
+        ListNode* endFirst = endOfFirstHalf(head);
+        ListNode *secondHalfHead = reverseList(endFirst->next);
+        
+        auto p1 = head, p2 = secondHalfHead;
+        while(p2!=NULL){
+            if(p1->val != p2->val)return false;
+            p1 = p1->next;
+            p2 = p2->next;
         }
         return true;
     }
