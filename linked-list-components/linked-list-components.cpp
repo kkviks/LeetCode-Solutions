cfc0e6c8d2b2059c6e201f3d1bc3ceb5233db290
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-06-13 16:28:49 +0530

    Time: 32 ms (88.72%), Space: 21.5 MB (57.03%) - LeetHub

--- linked-list-components/linked-list-components.cpp
+++ linked-list-components/linked-list-components.cpp
@@ -9,41 +9,15 @@
  * };
  */
 class Solution {
-public: 
-        
-    int find(int i, int parent[]){
-        if(parent[i]==-1)return i;
-        return parent[i]=find(parent[i],parent);
-    }
-    
-    void unite(int a,int b,int parent[],int rank[]){
-        int s1 = find(a,parent);
-        int s2 = find(b,parent);
-        if(s1!=s2){
-            if(rank[s2]>rank[s1])swap(s1,s2);
-            parent[s2] = s1;
-            rank[s1]+=rank[s2];
-        }
-    }
 public:
     int numComponents(ListNode* head, vector<int>& nums) {
-        int parent[10001], rank[10001];
-        for(int i=0;i<10001;i++){
-            parent[i] = -1;
-            rank[i] = 1;
-        }
         unordered_set<int> set(nums.begin(),nums.end());
-        int sz = 1;
-        while(head!=NULL and head->next!=NULL){
-          if(set.count(head->val) and set.count(head->next->val)){
-                unite(head->val,head->next->val,parent,rank);
-          } 
-          sz++;
-          head = head->next;
-        }
         int count = 0;
-        for(int i=0;i<sz;i++){
-            if(parent[i]==-1 and set.count(i))count++;
+        bool prevInSet = false;
+        while(head!=NULL){
+            if(prevInSet and set.count(head->val)==0)prevInSet = false;
+            else if(prevInSet==false and set.count(head->val))prevInSet=true,count++;
+            head = head->next;
         }
         return count;
     }
