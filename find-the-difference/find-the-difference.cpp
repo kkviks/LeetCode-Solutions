89300d76382aedfda7afd8dfa74c9bf932272f86
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-06-16 16:06:09 +0530

    Time: 4 ms (76.14%), Space: 6.6 MB (51.73%) - LeetHub

--- find-the-difference/find-the-difference.cpp
+++ find-the-difference/find-the-difference.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        
-        int ans = 0;
-        
-        for(int i=0;i<s.size();i++)
-        {
-            ans = ans^s[i]^t[i];
+        char ch = 0;
+        for(int i=0;i<s.size();i++){
+            ch^=s[i]^t[i];
         }
-        
-        return static_cast<char>(ans^t[s.size()]);        
+        return ch^t[t.size()-1];
     }
 };
