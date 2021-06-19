12e063c889ae116b724aefe702a582daa5f231be
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-06-19 19:55:33 +0530

    Time: 52 ms (5.10%), Space: 106.9 MB (5.44%) - LeetHub

--- n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
+++ n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
@@ -1,23 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-    }
-
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
 class Solution {
 private:
     vector<int> out;
@@ -26,10 +6,11 @@ public:
         if(root==NULL)return out;
         
         out.push_back(root->val);
-        for(auto child: root->children){
+        for(auto &child: root->children){
             preorder(child);
         }
         
         return out;
     }
 };
+ 
