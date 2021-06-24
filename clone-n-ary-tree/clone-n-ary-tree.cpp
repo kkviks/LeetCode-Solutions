b2711c60adadf1d5db6c5417c3371c7097184b33
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-06-24 16:52:02 +0530

    Time: 40 ms (87.19%), Space: 175.2 MB (57.64%) - LeetHub

---
+++ clone-n-ary-tree/clone-n-ary-tree.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneTree(Node* root) {
+        if(root==NULL)return NULL;
+        
+        Node* newNode = new Node(root->val);
+        
+        for(Node* child: root->children){
+            (newNode->children).push_back(cloneTree(child));
+        }
+        
+        return newNode;
+    }
+};
