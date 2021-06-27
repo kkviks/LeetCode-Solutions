aaf3213dba986385d65b8c62bf3bbb713d3f4fa0
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-06-27 21:29:27 +0530

    Time: 4 ms (74.52%), Space: 11.1 MB (28.14%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,31 +1,39 @@
 class Solution {
-private:
-    int indexOfMin(vector<int> &a){
-        int l = 0, n = a.size(), r = n-1;
-        while(l<r){
-            int mid = l+(r-l)/2;
-            if(a[mid]>a[r])l = mid+1;
-            else r = mid;
-        }
-        return l;
-    }
-    
-    int binarySearch(vector<int> &a,int l, int r, int x){
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(a[mid]==x)return mid;
-            else if(x>a[mid])l = mid+1;
-            else r = mid-1;
-        }
-        return -1;
-    }
 public:
     int search(vector<int>& a, int target) {
-        int min_idx = indexOfMin(a);
+        
         int n = a.size();
-        if(target>a[n-1]){
-            return binarySearch(a,0,min_idx-1,target);
+        int pivot;
+        
+        int l = 0, r = n;
+        
+        while(l<r){
+            int mid = l+(r-l)/2;
+            
+            if(a[mid]>a[n-1])
+                l=mid+1;
+            else
+                r=mid;
         }
-        return binarySearch(a,min_idx,n-1,target);
+        
+        pivot = l;
+        
+        if(target>a[n-1])
+            l=0, r = pivot-1;
+        else
+            l = pivot, r = n-1;
+        
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(a[mid]==target)
+                return mid;
+            if(target>a[mid])
+                l = mid+1;
+            else
+                r = mid-1;
+        }
+        
+        return -1;
+        
     }
 };
