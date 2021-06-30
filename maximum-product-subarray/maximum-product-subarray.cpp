560aac406818772b4df0012783767a1120baea6d
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-06-30 09:15:19 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (89.96%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.cpp
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int maxProduct(vector<int>& a) {
-        int n = a.size();
-        int dp[n][2];
         
-        dp[0][0] = max(0,a[0]);
-        dp[0][1] = min(0,a[0]);
+        if(a.size()==1)
+            return a[0];
         
-        if(n==1)return a[0];
+        int x = max(0,a[0]);
+        int y = min(0,a[0]);
         
-        int ans = dp[0][0];
+        int ans = x;
         
-        for(int i=1;i<n;i++){
-            if(a[i]>=0){
-                dp[i][0] = max(a[i],dp[i-1][0]*a[i]);
-                dp[i][1] = a[i]*dp[i-1][1];
-        
-            }else if(a[i]<0){
-                dp[i][0] = a[i]*dp[i-1][1];
-                dp[i][1] = min(a[i],a[i]*dp[i-1][0]);
+        for(int i=1;i<a.size();i++)
+        {
+            if(a[i]>=0)
+            {
+                x = max(a[i],a[i]*x);
+                y = y*a[i];
+            }else
+            {
+                int t = y;
+                y = min(a[i],x*a[i]);
+                x = t*a[i];
             }
-            ans = max(ans,dp[i][0]);
+            ans = max(ans,x);
         }
-
+        
         return ans;
     }
 };
