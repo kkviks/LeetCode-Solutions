9740619a22a7963929670137cfed5b9534f107e6
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-07-30 16:26:43 +0530

    Time: 12 ms (77.96%), Space: 12.5 MB (33.69%) - LeetHub

--- element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
+++ element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
-        double threshold = arr.size()/4.0;
-        int frq = 1;
-        int num = arr[0];
         
-        for(int i=1;i<arr.size();i++)
+        int n = arr.size();
+        vector<int> candidates = {arr[n/4],arr[n/2],arr[3*n/4]};
+        
+        for(int candidate: candidates)
         {
-            if(arr[i]==arr[i-1]){
-                frq++;
-            }else{
-                if(frq>threshold){
-                    return num;
-                }
-                frq = 1;
-                num = arr[i];
-            }
+            auto lb = lower_bound(arr.begin(),arr.end(),candidate);
+            auto ub = upper_bound(arr.begin(),arr.end(), candidate);
+            if(4*distance(lb,ub)>n)
+                return candidate;
         }
         
-        return num;
+        return -1;
     }
 };
