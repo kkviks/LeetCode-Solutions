bfa7dadf3820e053a47f4736f6a2f19d0a4cada9
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-08-04 19:30:34 +0530

    Time: 16 ms (91.02%), Space: 7.1 MB (83.34%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
         int max_size = 1;
-        string ans = s.substr(0,1);
+        string str = s.substr(0,1);
+        int start = 0;
         
-        for(int i=0;i<s.size();i++)
-        {
+        for(int i=0;i<s.size();i++){
             int l = i-1, r = i+1;
             int size = 1;
-            while(l>=0 and s[l]==s[r])
-            {
+            while(l>=0 and s[l]==s[r] and r<s.size()){
                 size+=2;
-                l--; r++;
-            }
-            if(size>max_size){
-                max_size = size;
-                ans = s.substr(l+1,size);
+                l--, r++;
             }
             
-            l = i, r = i+1;
-            size = 0;
-            while(l>=0 and r<s.size() and s[l]==s[r])
-            {
-                size+=2;
-                l--; r++;
-            }
             if(size>max_size){
                 max_size = size;
-                ans = s.substr(l+1,size);
+                start = l+1;
+            }
+            
+            l= i, r= i+1;
+            size = 0;
+            while(l>=0 and s[l]==s[r] and r<s.size()){
+                size+=2;
+                l--, r++;
+            }
+            
+            if(size>max_size){
+                max_size = size;
+                start = l+1;
             }
         }
         
-        cout << max_size << " ";
-        return ans;
+        return s.substr(start,max_size);
     }
 };
