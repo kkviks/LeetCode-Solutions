ba587547e22c8e3989aa532a2080f847406c44a2
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-08-15 12:56:02 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (21.54%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -14,43 +14,24 @@ public:
     vector<string> letterCombinations(string digits) {
         
         vector<string> ans;
-        solve(digits,ans);
+        if(digits.empty())
+            return ans;
+        ans.push_back("");
+        
+        for(char d: digits){
+            vector<string> temp;
+            for(char ch:m[d])
+            {
+                for(string s:ans)
+                {
+                    temp.push_back(s+ch);
+                }
+            }
+            ans = temp;
+        }
         
         return ans;
     }
     
-    void solve(string digits,vector<string> &ans)
-    {
-        if(digits.size()==0)
-            return;
-        
-        if(digits.size()==1){
-            string s = m[digits[0]];
-            for(char ch:s)
-            {
-                string temp(1,ch);
-                ans.push_back(temp);
-            }
-            return;
-        }
-        
-        solve(digits.substr(1),ans);
-        
-        string s = m[digits[0]];
-        vector<string> temp;
-        
-        for(char ch:s)
-        {
-            for(string curr:ans)
-            {
-                string t(1,ch);
-                temp.push_back(t+curr);
-            }
-        }
-        ans.clear();
-        for(auto x:temp)
-            ans.push_back(x);
-        
-        return;
-    }
+   
 };
