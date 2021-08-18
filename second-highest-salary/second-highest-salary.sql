2e7f1ecf7d6131031a8c45eae1d3a9f97dcd5f2f
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-08-18 15:05:11 +0530

    Time: 374 ms (99.54%), Space: 0B (100.00%) - LeetHub

--- second-highest-salary/second-highest-salary.sql
+++ second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,3 @@
-/* Write your PL/SQL query statement below */
-select max(salary) as SecondHighestSalary from employee
-where salary not in (select max(salary) from employee);
+SELECT MAX(SALARY) AS SecondHighestSalary 
+FROM Employee
+WHERE SALARY NOT IN (SELECT MAX(SALARY) from Employee);
