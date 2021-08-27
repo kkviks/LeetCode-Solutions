2c15d0bd2182f63be643400bd5277cfcebcfcb0e
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-08-27 14:35:30 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (54.55%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int x = 1, y= 1;
+        int dp[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
         for(int i=2;i<=n;i++){
-            int t = y;
-            y = x+y;
-            x= t;
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return y;
+        
+        return dp[n];
     }
 };
