1b680a6c258579585cfd6bafe489d69469149318
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-09-10 18:02:19 +0530

    Time: 8 ms (82.69%), Space: 12.7 MB (48.78%) - LeetHub

--- concatenation-of-array/concatenation-of-array.cpp
+++ concatenation-of-array/concatenation-of-array.cpp
@@ -3,13 +3,12 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> v(2*n);
+        nums.resize(2*n);
         
-        for(int i=0;i<n;i++)
-        {
-            v[i] = v[i+n] = nums[i];
+        for(int i=n;i<2*n;i++){
+            nums[i] = nums[i-n];
         }
         
-        return v;
+        return nums;
     }
 };
