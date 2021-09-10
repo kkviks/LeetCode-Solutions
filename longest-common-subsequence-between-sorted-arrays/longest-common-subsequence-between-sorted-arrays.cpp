9a4656e89f12ebc97460ee7308c472ac1da2d4c3
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2021-09-10 18:14:55 +0530

    Time: 24 ms (98.29%), Space: 15.6 MB (69.14%) - LeetHub

--- longest-common-subsequence-between-sorted-arrays/longest-common-subsequence-between-sorted-arrays.cpp
+++ longest-common-subsequence-between-sorted-arrays/longest-common-subsequence-between-sorted-arrays.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     vector<int> longestCommonSubsequence(vector<vector<int>>& arrays) {
-        vector<int> ans = f(arrays[0],arrays[1]);
+        int count[101] = {0};
         
-        for(int i=2;i<arrays.size();i++){
-            ans = f(ans,arrays[i]);
-        }
-        
-        return ans;
-    }
-    
-    vector<int> f(vector<int> &a, vector<int> &b){
-        int i = 0;
-        int j = 0;
-        
-        vector<int> lcs;
-        
-        while(i<a.size() and j<b.size()){
-            if(a[i]==b[j]){
-                lcs.push_back(a[i]);
-                i++; j++;
-            }else if(a[i]<b[j]){
-                i++;
-            }else{
-                j++;
+        for(auto &array: arrays){
+            for(int &num: array){
+                count[num]++;
             }
         }
         
-        return lcs;
+        vector<int> ans;
+        for(int i=0;i<101;i++){
+            if(count[i]==arrays.size()){
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
     }
 };
