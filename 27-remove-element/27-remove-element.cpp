2a144f45400f076a4bf531a5866d5f1531b5c24d
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2022-07-16 18:29:19 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (35.10%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -3,13 +3,10 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int rabbit = 0, turtle = 0;
         while(rabbit < nums.size()){
-            if(nums[rabbit]==val){
-                rabbit++;
-            }else{
-                swap(nums[rabbit], nums[turtle]);
-                rabbit++;
-                turtle++;
+            if(nums[rabbit]!=val){
+                swap(nums[rabbit], nums[turtle++]);
             }
+            ++rabbit;
         }
         return turtle;
     }
