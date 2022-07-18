db18f0f55b13fff50bb7979797e4cdd578ee5b4c
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2022-07-19 01:04:34 +0530

    Time: 6 ms (19.42%), Space: 6.6 MB (10.68%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        for(int i=s.size()-1;i>=0;--i){
+            if(s[i]==' '){
+                if(len>0)return len;
+            }else{
+                len++;
+            }
+        }
+        return len;
+    }
+};
