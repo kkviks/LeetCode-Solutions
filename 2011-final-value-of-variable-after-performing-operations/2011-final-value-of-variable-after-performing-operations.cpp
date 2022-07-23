9d92acfd5cd839bb8f0a8dd9fa93f0ce2b1fdb2f
Vikas Sheoran <56562130+kkviks@users.noreply.github.com>
2022-07-24 00:24:54 +0530

    Time: 11 ms (71.03%), Space: 14.1 MB (9.41%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int value = 0;
-        for(auto op: operations){
+        for(auto &op: operations){
             op[1]=='+'?++value:--value;
         }
         return value;
